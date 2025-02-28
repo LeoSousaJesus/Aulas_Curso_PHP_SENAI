3d1ee92f0fdf567b2b693dfa1237e684eb674dc6
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-02-28 20:54:16 -0300

    Adicionando Programa Contador

--- contador/result.php
+++ contador/result.php
@@ -1,8 +1,10 @@
 <?php
     //declarando variáveis
     $numero = $_POST['numero'];
+    $numero2 = $numero;
     
     //Laço de repetição while
+    echo "Repetição com while: <br/>";
     while ($numero >=0) {
         // exibe o número na tela
         echo $numero . "<br />";
@@ -11,4 +13,10 @@
         $numero--;
 
     }
+
+    echo "Repetiçõa com do...while<br/>";
+    do {
+        echo $numero2 . "<br />";
+        $numero2--;
+    } while ($numero2 >= 0);
 ?>
