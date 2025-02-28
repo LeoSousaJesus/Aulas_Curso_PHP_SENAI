7a6d67273c948621a96942640df6625a4fa008bb
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-02-28 20:46:17 -0300

    Adicionando Programa Contador

--- contador/result.php
+++ contador/result.php
@@ -1,4 +1,14 @@
 <?php
     //declarando variáveis
     $numero = $_POST['numero'];
+    
+    //Laço de repetição while
+    while ($numero >=0) {
+        // exibe o número na tela
+        echo $numero . "<br />";
+
+        // diminuir o valor do numero
+        $numero--;
+
+    }
 ?>
