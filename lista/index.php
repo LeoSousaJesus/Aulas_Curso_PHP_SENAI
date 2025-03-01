f3fca2bc56289e564d95a1b9d9360a73cde6e556
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-02-28 21:16:05 -0300

    Adicionando Programa Lista

---
+++ lista/index.php
@@ -0,0 +1,12 @@
+<?php
+    // declaração de array
+    $cidades = ["Brasília", "Goiânia", "Belo Hosrizonte", "São Paulo", "Unaí"];
+
+    // exibe  na tela o primeiro valor
+    //echo $cidade[0];
+
+    //percorrendo o array e exibindo os valores na tela
+    foreach ($cidades as $cidade) {
+        echo $cidade . "<br />";
+    }
+?>
