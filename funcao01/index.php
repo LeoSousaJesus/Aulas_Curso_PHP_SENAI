cb2e59c6c631813a01af0884b8a17be7720be2ad
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-07 19:42:19 -0300

    Adicionando Programa Função 01

---
+++ funcao01/index.php
@@ -0,0 +1,12 @@
+<?php
+    //função
+    function darBoasVindas(){
+        // escrever as ações da função
+
+        $nomeProgramador = "Leandro Sousa";
+        echo "Seja Bem Vindo, " . $nomeProgramador . ".";
+    }
+
+    //executando a função
+    darBoasVindas();
+?>
