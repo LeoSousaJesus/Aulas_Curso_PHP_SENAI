21830f37803b6762bfc1b82c070c548387935823
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-10 19:40:16 -0300

    Adicionando Programa Calculadora 02

--- calculadora02/resolucao.php
+++ calculadora02/resolucao.php
@@ -17,7 +17,36 @@
 * Após o usuário escolher o calculo, o programa retorna o resultado desejado.
 */
 
+// função do quadrilátero
+function calcularQuadrilatero($a, $b){
+    return -$b *$a
+}
+
+// função do triângulo
+function calcularTriangulo($a, $b){
+    return ($b * $a)/2;
+}
+
+//função da equação do 2º grau
 function calcularEqGrau1($a, $b){
-    return -$b/$a
+    return -$b/$a;
+}
+
+//declaração de variáveis$
+$n1 = str_ireplace(",",".", $_POST[n1]);
+$n2 = str_ireplace(",",".", $_POST[n2]);
+$operacao = $_POST['operacao'];
+
+//estrutura escolha
+switch ($operacao){
+    case "quadrilatero":
+        echo "Área do qudrilátero é " . calcularQuadrilatero($n1, $n2) . ".";
+        break;
+    case "triângulo":
+        echo "Área do triângulo é " . calcularTriangulo($n1, $n2) . ".";
+        break;
+    case "Equação de 1º grau":
+        echo "A equação do 1º grau é " . calcularEqGrau1($n1, $n2) . ".";
+        break;
 }
 ?>
