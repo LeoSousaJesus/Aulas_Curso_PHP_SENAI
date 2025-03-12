a5657b96c61eb01b151edadc87abcef5386a1f02
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-12 19:56:17 -0300

    Adicionando Programa Dados de Usuário com PHP

---
+++ dados_usuario/index.php
@@ -0,0 +1,4 @@
+<!--Crie um novo projeto chamado "dados_usuario", e nele crie um arquivo chamado "index.php". onde voçê ira criar um formulario onde o usuário irá preencher os seguintes dados:
+- Nome, Idade,  E-mail, Telefone, CPF, Profissão, CEP, UF,Cidade, Bairro, Logradouro, Complemento, número
+os dados devem ser armazenados em uma array e apresentados na tela. */ -->
+
