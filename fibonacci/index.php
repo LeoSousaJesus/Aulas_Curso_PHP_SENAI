27456f4ca12f123fe7c0709d0f839282c39ea8d4
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-14 20:37:00 -0300

    Adicionando Programa Fatorial com PHP

--- fibonacci/index.php
+++ fibonacci/index.php
@@ -3,14 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fatorial</title>
-    <style> body{
-                background:rgb(168, 216, 158);
-            }
-    </style>
+    <title>Fibonacci</title>
 </head>
 <body>
-    <h1>Cálculo do Fatorial de um número inteiro</h1>
+    <h1>Sequência de Fibonacci</h1>
     <hr />
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
         <label for="numero">Número:</label><br />
@@ -20,9 +16,9 @@
 </html>
 
 <?php
-    // função para calcular o fatorial
-    function calcularFatorial($numero) {
-        return $numero == 0 ? 1 : $numero * calcularFatorial($numero - 1);
+    // função para calcular a sequência de Fibonacci
+    function calcularFibonacci($numero) { 
+        return $numero < 2 ? $numero : calcularFibonacci($numero - 1) + calcularFibonacci($numero - 2);
     }
 
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
@@ -30,6 +26,5 @@
         $numero = $_POST['numero'];
 
         //saída de dados
-        echo "<h2>O fatorial de " . $numero . "! é: " . calcularFatorial ($numero) . "</h2>";
-        }  
-?>
+        echo "<h2>O número de Fibonacci de " . $numero . " é: " . calcularFibonacci($numero) . "</h2>";
+    }
