47190f6c9d241879786796bfac29e74f294fd301
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-21 19:30:19 -0300

    Adicionando Programa Construtor em PHP

---
+++ construtor/Pessoa.class.php
@@ -0,0 +1,10 @@
+<?php
+    // class Pessoa
+    class Pessoa {
+        // Atributos
+        public $nome;
+        public $idade;
+        public $email;
+        public $cargo;
+    }
+?>
---
+++ construtor/result.php
@@ -0,0 +1,7 @@
+<?php
+    // Importa a classe
+    include 'Pessoa.class.php';
+
+    // instancia a classe Pessoa
+    $usuario = new Pessoa();
+?>
