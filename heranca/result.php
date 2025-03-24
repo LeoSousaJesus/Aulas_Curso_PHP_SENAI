34ea9fa5b0014879cf265659be5ce2109b0b1c42
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-03-24 20:58:07 -0300

    Adicionando Programa Heranca em PHP

--- heranca/result.php
+++ heranca/result.php
@@ -7,12 +7,21 @@
     $usuario = new PessoaFisica();
 
     // atribui valores aos atributos
+    $usuario->setNome("Leonel");
+    $usuario->setCpf("123.456.789-10");
+    $usuario->setDataNascimento("28/02/1991");
+    $usuario->setProfissao("Programador");
     $usuario->setEmail("leo@gmail.com");
     $usuario->setTelefone("(61) 99234-5678");
     $usuario->setEndereco("Rua 400, Lote 45, Bairro Novo");
 
     // exibe os valores dos atributos
+    echo "<b>Nome:</b> " . $usuario->getNome() . "<br>";
+    echo "<b>CPF:</b> " . $usuario->getCpf() . "<br>";
+    echo "<b>Data de Nascimento:</b> " . $usuario->getDataNascimento() . "<br>";
+    echo "<b>Profissão:</b> " . $usuario->getProfissao() . "<br>";
     echo "<b>Email:</b> " . $usuario->getEmail() . "<br>";
     echo "<b>Telefone:</b> " . $usuario->getTelefone() . "<br>";
     echo "<b>Endereço:</b> " . $usuario->getEndereco() . "<br>";
+
 ?>
